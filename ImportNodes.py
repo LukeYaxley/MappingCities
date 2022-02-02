410631b99efc55c672cb93f03b82a9e4c10522b5
L Yaxley <ly105@student.le.ac.uk>
2022-02-02 12:57:14 +0000

    Updated ImportNodes.py to reduce the amount of time to get the nodes by cutting down on the "Roads" that we need to get

--- ImportNodes.py
+++ ImportNodes.py
@@ -3,12 +3,35 @@ import overpy
 api = overpy.Overpass()
 def get_roads(city_name):
 # function to fetch all ways and nodes
-    result = api.query(f"""
-    [out:json];
-    area[name = "{city_name}"];
-    way(area) ["highway"];
+    try:
+        result = api.query(f"""
+            [timeout:60][out:json];
+        area[name ="{city_name}"];
+        (
+        way(area)
+        ['name']
+        ['highway']
+        ['highway' !~ 'path']
+        ['highway' !~ 'steps']
+        ['highway' !~ 'motorway']
+        ['highway' !~ 'motorway_link']
+        ['highway' !~ 'raceway']
+        ['highway' !~ 'bridleway']
+        ['highway' !~ 'proposed']
+        ['highway' !~ 'construction']
+        ['highway' !~ 'elevator']
+        ['highway' !~ 'bus_guideway']
+        ['highway' !~ 'footway']
+        ['highway' !~ 'cycleway']
+        ['foot' !~ 'no']
+        ['access' !~ 'private']
+        ['access' !~ 'no'];
+        );
         (._;>;);
         out body;
-        
-        """)
-    return result.ways
+                """)
+        return result.ways
+    except overpy.exception.OverpassTooManyRequests:
+        print("City has too many nodes for city to handle")
+    finally:
+        return None
