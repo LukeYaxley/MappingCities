7f1fb9c7c586169e0dcbecf3129c292e2ec3f386
LukeYaxley <Ly105@student.le.ac.uk>
2022-02-19 16:32:26 +0000

    Ditched model as no need to store data, Instead we now just post the variable through to the results page

--- MappingCities/urls.py
+++ MappingCities/urls.py
@@ -20,6 +20,7 @@ from django.urls import path
 from .views import HomePageView, SearchResultsView
 
 urlpatterns = [
-    path('search/', SearchResultsView.as_view(), name='search_results'),
+    path('search/', SearchResultsView, name='search_results'),
     path('', HomePageView.as_view(), name='home'),
+
 ]
--- MappingCities/views.py
+++ MappingCities/views.py
@@ -1,22 +1,19 @@
-import django
-from django.http import HttpResponse
-import datetime
-import django.middleware.csrf as csrf
-from django.shortcuts import redirect
-import django.template.loader
-from django.views.decorators.csrf import csrf_protect
+
+from django.shortcuts import redirect, render
 
 import ImportNodes
 
-
 from django.views.generic import TemplateView, ListView
 
-from .models import City
 
 
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+    template_name = 'Home.html'
 
-class SearchResultsView(ListView):
-    model = City
+
+def SearchResultsView(request):
+    query = request.GET.get('name')
+    Ways = ImportNodes.get_roads(query)
     template_name = 'search_results.html'
+    return render(request, template_name, {'Ways': Ways})
+
