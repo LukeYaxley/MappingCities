9d285c8007c2d908c2944e324744e376848de632
H Palresha <hp345@student.le.ac.uk>
2022-03-15 21:51:19 +0000

    Updated PlotWays.py to extract List Co-ordinates of selective Road networks and let it process through the function in views.py to hopefully increase the efficiency of the code.

--- PlotWays.py
+++ PlotWays.py
@@ -1,33 +1,20 @@
-from ImportNodes import get_roads
 import pandas as pd
-import json
 import geopandas as gpd
-from shapely.geometry import Polygon
+
 
 def RoadsDataframe(city_name):
     # roads = get_roads(city_name)
     roads = city_name
     Nodes=[]
-    lat_list=[]
-    lon_list=[]
-    lat_list2=[]
-    lon_list2=[]
+
     for i in roads:
         for j in i.nodes:
-            Nodes.append([float(j.lon),float(j.lat),i.name, i.tags])
-        if (i.tags["highway"]=='primary'):
-            lat_list.append(float(j.lat))
-            lon_list.append(float(j.lon))
-        if (i.tags["highway"]=='secondary'):
-            lat_list2.append(float(j.lat))
-            lon_list2.append(float(j.lon))
+            Nodes.append([float(j.lat),float(j.lon),i.name, i.tags])
 
-    RoadsData = pd.DataFrame(Nodes, columns=["longitude","latitude","roadName","tags"])
-    polygon_geom = zip(lat_list,lon_list)
-    polygon_geom2 = zip(lat_list2, lon_list2)
+    RoadsData = pd.DataFrame(Nodes, columns=["latitude","longitude","roadName","tags"])
 
     # crs = {'init': 'epsg:4326'}
     # polygon = gpd.GeoDataFrame(index=[0], crs=crs, geometry=[polygon_geom])
-    return polygon_geom,polygon_geom2
+    return RoadsData
 
 # RoadsDataframe("Dereham")
